5b00bd39e0349e9f895cf6414e682489c105b310
Ramon Oestreich <ramon.oestreich@gmail.com>
2020-11-10 21:20:01 -0300

    add seeder objects

--- database/seeders/Product.ts
+++ database/seeders/Product.ts
@@ -1,8 +1,25 @@
+import  Product  from 'App/Models/Product';
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import {ProductFactory} from 'Database/factories'
+
 
 export default class ProductSeeder extends BaseSeeder {
   public async run () {
-     await ProductFactory.create()
+     await Product.createMany([
+      { 
+        name: 'Borracha Escolar Faber Castell Super Soft',
+        category: 'Bazar E Utilidades',
+        price:5.39,
+        brand: 'Faber Castell',
+        pathImage: 'https://savegnago.vteximg.com.br/arquivos/ids/293238_2'
+      },
+      { 
+        name: 'Cereal Barra Linea 60g Cookies Cream',
+        category: 'Mercearia',
+        price:4.99,
+        brand: 'Linea',
+        pathImage: 'https://savegnago.vteximg.com.br/arquivos/ids/296943_2'
+      }
+
+     ])
   }
 }
