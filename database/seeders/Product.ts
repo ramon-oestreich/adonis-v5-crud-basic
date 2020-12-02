53e3613a5d0cbe5f8d320f5e9244fd0576386ae9
Ramon Oestreich <ramon.oestreich@gmail.com>
2020-12-02 20:40:55 -0300

    solved factory

--- database/seeders/Product.ts
+++ database/seeders/Product.ts
@@ -1,8 +1,10 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import {ProductFactory} from 'Database/factories'
+import { ProductFactory } from 'Database/factories'
 
 export default class ProductSeeder extends BaseSeeder {
-  public async run () {
-     await ProductFactory.create()
+  public static developmentOnly = true
+
+  public async run() {
+    await ProductFactory.createMany(200)
   }
 }
